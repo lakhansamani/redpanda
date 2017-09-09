1a720ed2f2dba8bfa66455b2183ee925e12c2091
lakhansamani <lakhan.m.samani@gmail.com>
2017-09-09 11:16:31 +0530

    Adds scroll and route change handler for about us

--- src/components/views/AboutUs/index.js
+++ src/components/views/AboutUs/index.js
@@ -3,28 +3,38 @@ import { browserHistory } from 'react-router';
 import ContactUs from '../../partials/ContactUs'; // eslint-disable-line
 import Team from '../../partials/Team'; // eslint-disable-line
 import Profile from '../../partials/Profile'; // eslint-disable-line
-
+import scrollTo from '../../../lib/animatedScroll';
 class AboutUs extends React.Component {
   constructor(props) {
     super(props);
     this.handleScroll = this.handleScroll.bind(this);
   }
   componentDidMount() {
+    const path = this.props.location.pathname.split('/');
+    const element = document.getElementById(path['2']);
+    scrollTo(document.body, element.offsetTop - 50, 500);
     window.addEventListener('scroll', this.handleScroll);
   }
+  componentWillUnmount() {
+    window.removeEventListener('scroll');
+  }
   handleScroll(e) {
-    console.log('scrolling');
+    const topScroll = document.body.scrollTop;
     const teamDiv = document.getElementById('team');
     const profileDiv = document.getElementById('profile');
-    const contactDiv = document.getElementById('contactus');
-    console.log(teamDiv.scrollHeight, profileDiv.scrollTop, contactDiv.scrollTop);
-    if (teamDiv.srcollTop > 100) {
+    if (topScroll < profileDiv.scrollHeight) {
+      browserHistory.push('/aboutus/profile');
+    }
+    if (topScroll > (teamDiv.offsetTop - 100) && topScroll < teamDiv.scrollHeight) {
       browserHistory.push('/aboutus/team');
+    }
+    if (topScroll > (teamDiv.scrollHeight + 100 )) {
+      browserHistory.push('/aboutus/contactus');
     }
   }
   render() {
     return (
-      <div id="about-us">
+      <div ref="aboutUs">
         <h1> AboutUs </h1>
         <Profile />
         <Team />
