7035118a472c84f5500fc1e6535109f27093fadc
lakhansamani <lakhan.m.samani@gmail.com>
2017-09-09 22:49:01 +0530

    Fixes login css

--- src/lib/authLib.js
+++ src/lib/authLib.js
@@ -1,22 +1,20 @@
 import { browserHistory } from 'react-router';
-module.exports = {
-  isLoggedIn: () => {
-    if (window.localStorage.getItem('authData')) {
-      return true;
-    }
-    return false;
-  },
-  getUser: () => JSON.parse(window.localStorage.getItem('authData') || '{}'),
-  requireAuth: (nextState, replace) => {
-    if (!window.localStorage.getItem('authData')) {
-      replace({
-        pathname: '/',
-        state: { nextPathname: nextState.location.pathname }
-      });
-    }
-  },
-  logout: () => {
-    window.localStorage.removeItem('authData');
-    browserHistory.push('/');
+export const isLoggedIn = () => {
+  if (window.localStorage.getItem('authData')) {
+    return true;
   }
+  return false;
+};
+export const getUser = () => JSON.parse(window.localStorage.getItem('authData') || '{}');
+export const requireAuth = (nextState, replace) => {
+  if (!window.localStorage.getItem('authData')) {
+    replace({
+      pathname: '/',
+      state: { nextPathname: nextState.location.pathname }
+    });
+  }
+};
+export const logout = () => {
+  window.localStorage.removeItem('authData');
+  browserHistory.push('/');
 };
